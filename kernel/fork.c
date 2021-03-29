00e32166b05764c3f608e3a8d2f23de85a0d173e
Peter Zijlstra <peterz@infradead.org>
2021-03-29 15:18:35 +0200

    sched: Inherit task cookie on fork()
    Note that sched_core_fork() is called from under tasklist_lock, and
    not from sched_fork() earlier. This avoids a few races later.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Don Hiatt <dhiatt@digitalocean.com>
    Tested-by: Hongyu Ning <hongyu.ning@linux.intel.com>
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210422123308.980003687@infradead.org
    (cherry picked from commit 85dd3f61203c5cfa72b308ff327b5fbf3fc1ce5e)
    (cherry picked from commit 3bb61049054b05e29571a46c7eb4b73f8c232b30)
    (cherry picked from commit d1c3fe347491bf03fd777174e368f849526c4afc)
    (cherry picked from commit 89cdb544bf9ee6e857bd9882b15b79d2a998f888)
    (cherry picked from commit 3afbc602f7a20d5b9cf68c49ab8293fe2c07a6fd)

--- kernel/fork.c
+++ kernel/fork.c
@@ -2296,6 +2296,8 @@ static __latent_entropy struct task_struct *copy_process(
 
 	klp_copy_process(p);
 
+	sched_core_fork(p);
+
 	spin_lock(&current->sighand->siglock);
 
 	/*
@@ -2383,6 +2385,7 @@ static __latent_entropy struct task_struct *copy_process(
 	return p;
 
 bad_fork_cancel_cgroup:
+	sched_core_free(p);
 	spin_unlock(&current->sighand->siglock);
 	write_unlock_irq(&tasklist_lock);
 	cgroup_cancel_fork(p, args);
